19dd17b7b0940d01e17e6ea6ba1afb3c0193eda1
Enzo Mourany <enzo.mourany@outlook.com>
2022-04-03 22:16:59 +0200

    created user

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const userRoutes = require('./router/user-routes');
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
 const app = express();
 
+// routes
+app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
 })
